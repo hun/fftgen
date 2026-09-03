08e736abba556743f62fe65604f2c44aae54559a
Hannes Klas <hannes.klas@gmail.com>
2026-09-03 08:11:16 +0200

    S8: N=32768 chain bringup PASS (delta 0, first 4096 outputs); 65536 sim in background

--- spikes/S7_r23/rtl_dit/tb_chain_dit.v
+++ spikes/S7_r23/rtl_dit/tb_chain_dit.v
@@ -1,8 +1,8 @@
 `timescale 1ns/1ps
 module tb;
     localparam integer W = 16;
-    localparam integer N = 32768;
-    localparam integer LAT = 32772;
+    localparam integer N = 65536;
+    localparam integer LAT = 65550;
     localparam integer TOTAL = 2*N;
     reg clk = 0, rst = 1;
     reg tvalid = 1, tuser = 0, tlast = 0;
@@ -13,10 +13,10 @@ module tb;
     wire signed [W-1:0] ore, oim;
     wire muser, mlast;
     fft_sdf_r23_dit #(
-        .NUM_POINTS(32768), .SAMPLE_WIDTH(16), .INTERN_WIDTH(16),
+        .NUM_POINTS(65536), .SAMPLE_WIDTH(16), .INTERN_WIDTH(16),
         .TWIDDLE_WIDTH(18), .TWIDDLE_DECIMAL(17),
-        .SCALING_PACK(64'h15555555),
-        .INVERSE(1), .TWIDDLE_FILE("/home/hannes/Projects/fftgen/spikes/S7_r23/rtl_dit/tw_chain_32768.mem"), .Q8(92682),
+        .SCALING_PACK(64'h55555555),
+        .INVERSE(1), .TWIDDLE_FILE("/home/hannes/Projects/fftgen/spikes/S7_r23/rtl_dit/tw_chain_65536.mem"), .Q8(92682),
         .R2B_I(16'hffff), .R2B_C(1'h0),
         .R2B_WPTR(16'hffff), .R2B_PWP(16'hffff),
         .R2B_RADDR(16'hffff), .R2B_PIPE(9'h0)
@@ -28,8 +28,8 @@ module tb;
            .m_axis_tuser(muser), .m_axis_tlast(mlast));
     always #5 clk = ~clk;
     initial begin
-        $readmemh("/home/hannes/Projects/fftgen/spikes/S7_r23/rtl_dit/stim_chain_32768.txt", stim);
-        fd = $fopen("/home/hannes/Projects/fftgen/spikes/S7_r23/rtl_dit/rtl_chain_32768.txt", "w");
+        $readmemh("/home/hannes/Projects/fftgen/spikes/S7_r23/rtl_dit/stim_chain_65536.txt", stim);
+        fd = $fopen("/home/hannes/Projects/fftgen/spikes/S7_r23/rtl_dit/rtl_chain_65536.txt", "w");
         repeat (2) @(negedge clk);
         rst = 0;
         for (c = 0; c < TOTAL + LAT + 4; c = c + 1) begin
